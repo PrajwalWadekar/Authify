8eddc7f1e313fa04b6c2e4770fde79a3395f3a9e
Prajwal Wadekar <prajwalvwadekar@gmail.com>
2025-07-14 20:45:01 +0530

    Completed backend setup & implemented check auth

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+
 import { connectDB } from "./db/connectDB.js"
 import authRoutes from './routes/auth.routes.js';
 // import otpRoutes from './routes/email.routes.js';
@@ -9,7 +11,9 @@ dotenv.config();
 const app=express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json()); //allows us to parse invoming request : req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
+
 app.use("/api/auth",authRoutes);
 
 
